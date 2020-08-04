94362e3c7dd56199e94d8271b38ae1d68d549de5
Paul <webdevpaulmason@gmail.com>
2020-08-04 15:12:20 -0700

    Update Methods to be outside state

--- src/components/App.js
+++ src/components/App.js
@@ -13,49 +13,78 @@ class App extends Component {
         values: '2,3,4,5,6,7,8,9,10,J,Q,K,A',
         playerCurrentCard: '',
         computerCurrentCard: '',
-        initializeDeck(){
-          const {suits, values, deck } = this;
-          for(let value of values.split(',')){
-            for(let suit of suits){
-              deck.push(value + suit);
-            }
-          } 
-        },
-        shuffle() {
-          const { deck } = this;
-          for(let i = deck.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [deck[i], deck[j]] = [deck[j], deck[i]];
-          }
-        },
-        dealPlayers() {
-          const { deck, playerDeck, computerDeck } = this;
-          for(let i = 0; i < 26; i ++){
-            let playerCard = deck.pop();
-            playerDeck.push(playerCard);
-            let computerCard = deck.pop();
-            computerDeck.push(computerCard);
-          }
-        }
       }
     }
   }
 
+  initializeDeck = () => {
+    const {suits, values,} = this.state.game;
+    const localDeck = [];
+    for(let value of values.split(',')){
+      for(let suit of suits){
+        localDeck.push(value + suit);
+      }
+    } 
+    this.setState((prevState) => ({
+      prevState,
+      game: {...prevState.game, deck: [...prevState.game.deck, ...localDeck]}
+    }));
+  }
+
+  shuffle() {
+    const { deck } = this.state.game;
+    const localShuffle = [...deck];
+    for(let i = localShuffle.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [localShuffle[i], localShuffle[j]] = [localShuffle[j], localShuffle[i]];
+      console.log(localShuffle);
+    }
+    
+    this.setState((prevState) => ({
+      prevState,
+      game: {...prevState.game, deck: [...prevState.game.deck, ...localShuffle]}
+    }));
+  }
+
+  dealPlayers() {
+    const { deck } = this.state.game;
+    const localPlayerDeck = [];
+    const localComputerDeck = [];
+    for(let i = 0; i < 26; i ++){
+      let playerCard = deck.pop();
+      localPlayerDeck.push(playerCard);
+      let computerCard = deck.pop();
+      localComputerDeck.push(computerCard);
+    }
+    this.setState((prevState)=> ({
+      prevState,
+      game: {...prevState.game, playerDeck: [...prevState.game.playerDeck, ...localPlayerDeck]},
+    }));
+    this.setState((prevState)=> ({
+      prevState,
+      game: {...prevState.game, computerDeck: [...prevState.game.computerDeck, ...localComputerDeck]},
+    }));
+  }
+
   componentDidMount(){
-    this.state.game.initializeDeck();
-    // this.state.game.shuffle();
-    // this.state.game.dealPlayers();
-    console.log(this.state.game.deck[0]);
-    console.log(this.state.game.deck);
-    console.log(this.state.game.playerDeck);
-    console.log(this.state.game.computerDeck);
+    this.initializeDeck();
+    this.shuffle();
+    // this.dealPlayers();
+    console.log(this.state.game.playerDeck[0]);
+    // console.log(this.state.game.deck);
+    // console.log(this.state.game.playerDeck);
+    // console.log(this.state.game.computerDeck);
+    
   }
 
   render() {
+    console.log(this.state.game.deck);
+    console.log(this.state.game.playerDeck);
+    console.log(this.state.game.computerDeck);
+    
     return(
       <div>
-        <Card cardName="AD"/>
-        <h1>{this.state.game.deck.length ? this.state.game.deck[0] : null}</h1>
+        <Card cardName={this.state.game.deck[25]}/>
       </div>
     );
   }
