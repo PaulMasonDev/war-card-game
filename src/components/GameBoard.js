b77792a4e94296fc5f71b1489a8fe5ac7bd0609c
Paul <webdevpaulmason@gmail.com>
2020-08-05 09:46:30 -0700

    Refactor pass

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -11,13 +11,27 @@ class GameBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      game: {
         deck: [], // Initialized Deck
         playerDeck: [],
         computerDeck: [],
         tempDeck: [], // Used when there is a tie to hold the cards that the next winner will get.
-      },
     };
+  }
+
+  // Main deck update function
+  deckUpdate = (tempDeck, stateDeck = 'deck') => {
+    this.setState((prevState) => ({
+      prevState,
+      [stateDeck]: [...prevState[stateDeck], ...tempDeck],
+    }));
+  }
+
+  // Fresh deck update function
+  freshDeckUpdate = (tempDeck, stateDeck = 'deck') => {
+    this.setState((prevState) => ({
+      prevState,
+      [stateDeck]: [...tempDeck],
+    }));
   }
 
   initializeDeck = () => {
@@ -29,135 +43,86 @@ class GameBoard extends Component {
         localDeck.push(value + suit);
       }
     }
-    this.setState((prevState) => ({
-      //Setting proper state with the temporary localDeck
-      prevState,
-      game: { ...prevState.game, deck: [...prevState.game.deck, ...localDeck] },
-    }));
+    this.deckUpdate(localDeck);
   };
 
   shuffle() {
-    const { deck } = this.state.game;
+    const { deck } = this.state;
     const localShuffle = [...deck];
     for (let i = localShuffle.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [localShuffle[i], localShuffle[j]] = [localShuffle[j], localShuffle[i]];
     }
-
-    this.setState((prevState) => ({
-      prevState,
-      game: { ...prevState.game, deck: [...localShuffle] },
-    }));
+    this.freshDeckUpdate(localShuffle);
   }
 
   dealPlayers() {
-    const { deck } = this.state.game;
+    const { deck } = this.state;
     const localPlayerDeck = [];
     const localComputerDeck = [];
     for (let i = 0; i < 26; i++) {
       // Deals out all cards from deck and since shuffled, will be random cards in each deck.
-      let playerCard = deck.pop(); // Draw from the deck
-      localPlayerDeck.push(playerCard); // Deal to player
-      let computerCard = deck.pop(); // Draw from the deck
-      localComputerDeck.push(computerCard); //Deal to the Computer
+      function deal(dealDeck){
+        dealDeck.push(deck.pop()); // Draw from the deck and deal to player/comp.
+      }
+      deal(localPlayerDeck);
+      deal(localComputerDeck);
     }
-    this.setState((prevState) => ({
-      ...prevState,
-      game: {
-        ...prevState.game,
-        playerDeck: [...prevState.game.playerDeck, ...localPlayerDeck],
-        computerDeck: [...prevState.game.computerDeck, ...localComputerDeck],
-      },
-    }));
+    this.deckUpdate(localPlayerDeck, 'playerDeck');
+    this.deckUpdate(localComputerDeck, 'computerDeck');
   }
 
   async startGame() {
     await this.initializeDeck();
     await this.shuffle();
     this.dealPlayers();
-    console.log(this.state.game.playerDeck[0]);
+    console.log(this.state.playerDeck[0]);
   }
   nextRound = () => {
-    // Grab the first character of each card in order to compare values. Some modification must be done to the non-numeric values and to 10, since it uses 0.
-    let player = this.state.game.playerDeck[0].slice(0, 1);
-    let computer = this.state.game.computerDeck[0].slice(0, 1);
+    // Grab the first character of each card in order to eventually compare values. Some modification must be done to the non-numeric values and to 10, since 10 uses 0 when referencing the image.
+    let player = this.state.playerDeck[0].slice(0, 1);
+    let computer = this.state.computerDeck[0].slice(0, 1);
     if (typeof player === "string" || player === "0") {
-      switch (player) {
-        case "A":
-          player = 14;
-          break;
-        case "J":
-          player = 11;
-          break;
-        case "Q":
-          player = 12;
-          break;
-        case "K":
-          player = 13;
-          break;
-        case "0":
-          player = 10;
-          break;
-        default:
-          player = this.state.game.playerDeck[0].slice(0, 1);
-      }
+      player = player === "A" ? 14 
+        : player === "J" ? 11 
+        : player === "Q" ? 12 
+        : player === "K" ? 13 
+        : player === "0" ? 10
+        : player;
     }
     if (typeof computer === "string" || computer === "0") {
-      switch (computer) {
-        case "A":
-          computer = 14;
-          break;
-        case "J":
-          computer = 11;
-          break;
-        case "Q":
-          computer = 12;
-          break;
-        case "K":
-          computer = 13;
-          break;
-        case "0":
-          computer = 10;
-          break;
-        default:
-          computer = this.state.game.computerDeck[0].slice(0, 1);
-      }
+      computer = computer === "A" ? 14 
+        : computer === "J" ? 11 
+        : computer === "Q" ? 12 
+        : computer === "K" ? 13 
+        : computer === "0" ? 10
+        : computer;
     }
     //Temporary Computer and Player Arrays copied from state, and will later be put into state.
-    let tempCompDeck = [...this.state.game.computerDeck];
-    let tempPlayerDeck = [...this.state.game.playerDeck];
+    let tempCompDeck = [...this.state.computerDeck];
+    let tempPlayerDeck = [...this.state.playerDeck];
 
     if (player > computer) {
       // If player has a higher value.
       const card = tempCompDeck.shift(); // Take the card from the computer
       tempPlayerDeck.push(card); // Put it into the player deck
       tempPlayerDeck.push(tempPlayerDeck.shift()); // Put your own card back into deck.
-      if (this.state.game.tempDeck.length > 0) {
+      if (this.state.tempDeck.length > 0) {
         // If there were the same values drawn.
-        const tempDeck = [...this.state.game.tempDeck];
-        // This portion is not altogether working correctly.  Current Issue: Even though the length might be 6, only half the cards are pushed to the player or computer.
-        for (let i = 0; i < tempDeck.length; i++) {
-          let card = tempDeck.shift();
-          tempPlayerDeck.push(card);
-        }
+        const tempDeck = [...this.state.tempDeck];
         tempPlayerDeck = [...tempPlayerDeck, ...tempDeck];
-        this.setState((prevState) => ({
-          prevState,
-          game: { ...prevState.game, tempDeck: [] },
-        }));
+        this.freshDeckUpdate([], 'tempDeck');
       }
     } else if (computer > player) {
-      const card = tempPlayerDeck.shift();
-      tempCompDeck.push(card);
-      tempCompDeck.push(tempCompDeck.shift());
-      if (this.state.game.tempDeck.length > 0) {
-        const tempDeck = [...this.state.game.tempDeck];
-        tempCompDeck = [...tempCompDeck, ...tempDeck];
-        this.setState((prevState) => ({
-          prevState,
-          game: { ...prevState.game, tempDeck: [] },
-        }));
-      }
+        const card = tempPlayerDeck.shift(); // Take the card from the player.
+        tempCompDeck.push(card); // Put it into the computer deck.
+        tempCompDeck.push(tempCompDeck.shift()); // Put your own card back into deck.
+        if (this.state.tempDeck.length > 0) {
+          // If there were the same values drawn.
+          const tempDeck = [...this.state.tempDeck];
+          tempCompDeck = [...tempCompDeck, ...tempDeck];
+          this.freshDeckUpdate([], 'tempDeck');
+        }
     } else if (player === computer) {
       // If values are the same.
       const tempDeck = [];
@@ -166,26 +131,13 @@ class GameBoard extends Component {
           tempDeck.push(tempPlayerDeck.shift());
           tempDeck.push(tempCompDeck.shift());
         }
-        this.setState((prevState) => ({
-          prevState,
-          game: {
-            ...prevState.game,
-            tempDeck: [...prevState.game.tempDeck, ...tempDeck],
-          },
-        }));
+        this.deckUpdate(tempDeck, 'tempDeck');
       };
       dealCards(3);
     }
-    this.setState((prevState) => ({
-      prevState,
-      game: { ...prevState.game, playerDeck: [...tempPlayerDeck] },
-    }));
-    this.setState((prevState) => ({
-      prevState,
-      game: { ...prevState.game, computerDeck: [...tempCompDeck] },
-    }));
+    this.freshDeckUpdate(tempPlayerDeck, 'playerDeck');
+    this.freshDeckUpdate(tempCompDeck, 'computerDeck');
 
-    // console.log('TEMP Player:', tempPlayerDeck, 'TEMP Comp:', tempCompDeck)
     player > computer
       ? console.log("PLAYER WINS")
       : console.log("COMPUTER WINS");
@@ -200,17 +152,15 @@ class GameBoard extends Component {
   }
 
   render() {
-    console.log("GAME DECK: ", this.state.game.deck);
-    console.log("PLAYER DECK: ", this.state.game.playerDeck);
-    console.log("COMPUTER DECK: ", this.state.game.computerDeck);
-    console.log("TEMP DECK: ", this.state.game.tempDeck);
+    console.log("GAME DECK: ", this.state.deck);
+    console.log("PLAYER DECK: ", this.state.playerDeck);
+    console.log("COMPUTER DECK: ", this.state.computerDeck);
+    console.log("TEMP DECK: ", this.state.tempDeck);
 
-    const compData = this.state.game.computerDeck;
-    const playerData = this.state.game.playerDeck;
     return (
       <div>
-        <ComputerHand data={compData} />
-        <PlayerHand data={playerData} />
+        <ComputerHand data={this.state.computerDeck} />
+        <PlayerHand data={this.state.playerDeck} />
         <button onClick={this.handleClick}>Next Round</button>
       </div>
     );
